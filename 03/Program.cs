677056923551c54f1cc10a6fd5b2001f53953f54
Oleg <olegashepelev@gmail.com>
2022-12-21 21:25:35 +0600

    Complete third task

---
+++ 03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите день недели в виде числа от 1 до 7: ");
+string input = Console.ReadLine() ?? "";
+
+int number = int.Parse(input);
+
+if (number >= 1 && number <= 5)
+{
+    Console.Write($"{number} -> нет");
+}
+else
+{
+    Console.Write($"{number} -> да");
+}
