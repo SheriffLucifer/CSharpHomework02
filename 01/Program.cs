8c2ec15163dee9adbb4c65296947880416f3d804
Oleg <olegashepelev@gmail.com>
2022-12-21 21:17:52 +0600

    Complete two tasks

---
+++ 01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+string input = Console.ReadLine() ?? "";
+
+int number = int.Parse(input);
+
+int res = number / 10 % 10;
+
+Console.Write($"{number} -> {res}");
---
+++ 02/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите любое число: ");
+string input = Console.ReadLine() ?? "";
+
+int number = int.Parse(input);
+
+if (number > 99 && number <= 999)
+{
+    int res = number % 10;
+    Console.Write($"{number} -> {res}");
+}
+else if (number > 999 && number <= 9999)
+{
+    int res = number / 10 % 10;
+    Console.Write($"{number} -> {res}");
+}
+else if (number > 9999 && number <= 99999)
+{
+    int res = number / 100 % 10;
+    Console.Write($"{number} -> {res}");
+}
+else
+{
+    Console.Write($"{number} -> третьей цифры нет");
+}
